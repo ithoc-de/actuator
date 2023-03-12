f6d3c80e168248c079b0e399614dd413ba1b35d8
Oli Hock <oliver.hock@gmail.com>
2023-03-12 15:59:38 +0100

    add root access that returns spring boot version

---
+++ src/main/java/de/ithoc/actuator/PingController.java
@@ -0,0 +1,15 @@
+package de.ithoc.actuator;
+
+import org.springframework.core.SpringVersion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/")
+    public String root() {
+        return SpringVersion.getVersion();
+    }
+
+}

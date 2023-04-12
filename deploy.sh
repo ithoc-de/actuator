7d88656d536d6f0a6d6879778c7971d2506b31de
Oli Hock <oliver.hock@gmail.com>
2023-04-12 15:01:47 +0200

    configure default port number

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build --tag olihock/actuator:1.0.1 .
-docker run --name actuator -p 7003:7003 olihock/actuator:1.0.1
+docker run --name actuator -p 7003:8080 olihock/actuator:1.0.1
 
 #docker push olihock/actuator:1.0.1
 #kubectl apply -f k8s/actuator-deployment.yaml
